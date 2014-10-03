ea2e2e32a97c4ab51d083484d598fa68631f7c82
zwpwjwtz <zwpwjwtz@126.com>
2014-10-03 23:06:07 +0800

    添加数据库管理信息

---
+++ 数据库/数据库创建信息/Insert.sql
@@ -0,0 +1,3 @@
+use UsersOfOPENBBS;
+show tables;
+insert into BaseInfOfUsers values('0000004','srxh','123456','c:/Users',1,0,'null',2,19);
---
+++ 数据库/数据库创建信息/ViewOPENBBS.sql
@@ -0,0 +1,7 @@
+show databases;
+use UsersOfOPENBBS;
+show tables;
+describe BaseInfOfUsers;
+select * from BaseInfOfUsers;
+describe PostOfUsers;
+select * from PostOfUsers;
