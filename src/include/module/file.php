07fcb4d75e7cd924a67582fd3cdb459e96a0b4b3
henrys8 <547445609@qq.com>
2014-10-07 23:45:59 +0800

    添加数据库写入部分（头像）

--- src/include/module/file.php
+++ src/include/module/file.php
@@ -2,21 +2,29 @@
 
 define("USERFILE","./userfile/");
 
-//需要变量：id（用户的或帖子的）、附件名、附件类型、附件内容
+//功能：保存文件
+//需要变量：id（用户的或帖子的）、文件名、文件类型、文件内容
 //$file_type=1,头像；$file_type=2,附件
 function save_file($id,$file_name,$file_type,$file_content)
 {
-	$file_id=md5(time());//生成附件的md5
+	$file_id=md5(time());//生成文件的md5
 	//数据库检验$file_id是否重复，若重复则重新生成，最多重新生成50次
-	//数据库写入($id,$file_name,$file_type,$file_id);
+	//数据库写入($id,$file_id);
+	if($file_type==1){
+		$a=resetUserPicture($id,$file_id);
+		if($a==false)		
+			return false;//保存失败
+	}
+	
 	$fp=fopen(USERFILE.$file_id,"wb");
 	fwrite($fp,$file_content);
 	fclose($fp);
-	return true;
+	return true;//保存成功
 }
 
-//需要变量：附件md5、附件内容
-//附件内容保存在$file_content中
+//功能：读取文件
+//需要变量：文件md5、文件内容
+//文件内容保存在$file_content中
 function load_file($file_id,&$file_content)
 {
 	if(file_exists(USERFILE.$file_id)==false)
@@ -25,11 +33,12 @@ function load_file($file_id,&$file_content)
 		$fp=fopen(USERFILE.$file_id,"rb");
 		$file_content=fread($fp,filesize($file_id));
 		fclose($fp);
-		return 1;
+		return 1;//读取成功
 	}
 }
 
-//需要变量：附件md5
+//功能：删除文件
+//需要变量：文件md5
 function remove_file($file_id)
 {
 	if(file_exists(USERFILE.$file_id)==false)
