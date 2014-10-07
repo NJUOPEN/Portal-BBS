9a7cb4031324d3fd0216d2a3fb210a7a523c38ca
zwpwjwtz <zwpwjwtz@126.com>
2014-10-07 19:32:03 +0800

    将主键ID改为自动增长（AUTO INCREMEMNT）的int类型

--- 数据库/数据库创建信息/BuildOPENBBS.sql
+++ 数据库/数据库创建信息/BuildOPENBBS.sql
@@ -1,7 +1,7 @@
 create database UsersOfOPENBBS;
-show databases ;
+show databases;
 use UsersOfOPENBBS;
-create table BaseInfOfUsers(SysID varchar(7),Name varchar(10),Code varchar(16),Picture varchar(15),Root int(2),Rank int(8),
+create table BaseInfOfUsers(SysID int NOT NULL AUTO_INCREMENT,Name varchar(10),Code varchar(16),Picture varchar(15),Root int(2),Rank int(8),
 Blank varchar(5),Gender int(2),Age int(200),primary key(SysID));
-create table PostOfUsers(PostID varchar(7),IDofUsers varchar(7),Time varchar(20),IfFollow int(2),PostAdd varchar(20),
+create table PostOfUsers(PostID int NOT NULL AUTO_INCREMENT,IDofUsers varchar(7),Time varchar(20),IfFollow int(2),PostAdd varchar(20),
 FellowNum int(255),FellowAdd varchar(20),primary key(PostID),foreign key (IDofUsers) references BaseInfOfUsers (SysID));
