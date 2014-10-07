6f2855d202b631c5f3b7db4a68e6dff050aff8ed
Wonicon <conciselove@outlook.com>
2014-10-07 12:14:58 +0800

    全弄成函数了，SQL尚未驾驭

--- src/include/module/log.php
+++ src/include/module/log.php
@@ -7,39 +7,37 @@
 
 //参考:blog.csdn.net/sysprogram/article/details/21107041
 
-class log {
-	/**
-	 * 登录
-	 */
-	public function login($params) {
+
+if (!session_id()) session_start();
+
+function login($params) {
 		$username = $params['username'];
-		$password = sha1($params['password']);
+		$password = $params['password'];
 		//TODO 数据库存储的是明码还是hash值?
 		//连接数据库
-		include('SQL.php');
+		include('c:\src\inlucde\module\SQL.php');
 		$data = new SQL_Operator;
 		//认证
 		$check_result = $data->get('BaseInfOfUsers', $username, $password);
 		if(!empty($check_result)) { //登录成功
-   			session_start();
     		$_SESSION['username'] = $username;
     		$_SESSION['userid'] = $check_result['userid'];
-    		echo "登录成功<br />";
+    		echo 'Login success<br />';
     		exit;
 		} else {
-    		exit("登录失败<br />");
+    		exit("Login failed<br />");
 		}
 	}
 
 	/**
 	 * 登出
 	 */
-	public function logout() {
-    		unset($_SESSION['userid']);
-    		unset($_SESSION['username']);
-    		echo "注销成功<br />";
-    		exit;
+function logout() {
+	//echo "$_SESSION[userid]<br />";
+	//echo "$_SESSION[username]<br />";
+    unset($_SESSION['userid']);
+    unset($_SESSION['username']);
+    echo "Log out success<br />";
+    exit;
     }
-}
-
 ?>
