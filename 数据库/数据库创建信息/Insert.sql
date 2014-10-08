a62f143a857c01e50c0e7390467270cd3793097c
zwpwjwtz <zwpwjwtz@126.com>
2014-10-08 20:29:59 +0800

    更新index.php和config.php

--- 数据库/数据库创建信息/Insert.sql
+++ 数据库/数据库创建信息/Insert.sql
@@ -1,3 +1,3 @@
 use UsersOfOPENBBS;
 show tables;
-insert into BaseInfOfUsers values('4','srxh','7c4a8d09ca3762af61e59520943dc26494f8941b','c:/Users',1,0,'null',2,19);
+insert into BaseInfOfUsers values('4','srxh','7c4a8d09ca3762af61e59520943dc26494f8941b','',1,0,NULL,2,19);
