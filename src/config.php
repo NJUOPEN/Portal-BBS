1717ca2826f3bef333ed84024e8eb9826a178e30
zwpwjwtz <zwpwjwtz@126.com>
2014-10-08 19:34:42 +0800

    添加总的配制文件

---
+++ src/config.php
@@ -0,0 +1,13 @@
+<?php
+/*
+该文件定义整个论坛运行所需的常量，应当首先被加载
+*/
+
+define('BBS_ROOT',dirname(__FILE__));
+
+define('SQL_HOST','127.0.0.1');
+define('SQL_ACCOUNT','test');
+define('SQL_PASSWORD','password');
+define('SQL_DB','db');
+
+?>
--- 数据库/数据库创建信息/Insert.sql
+++ 数据库/数据库创建信息/Insert.sql
@@ -1,3 +1,3 @@
 use UsersOfOPENBBS;
 show tables;
-insert into BaseInfOfUsers values('0000004','srxh','123456','c:/Users',1,0,'null',2,19);
+insert into BaseInfOfUsers values('4','srxh','7c4a8d09ca3762af61e59520943dc26494f8941b','c:/Users',1,0,'null',2,19);
