1c52dee9b6dcb411bd3532f473d573215eb45657
zfy1696621890 <1696621890@qq.com>
2014-10-12 21:57:10 +0800

    Update InsertPost.sql

--- 数据库/数据库创建信息/InsertPost.sql
+++ 数据库/数据库创建信息/InsertPost.sql
@@ -1,2 +1,2 @@
-insert into PostOfUsers values(7,4,'2014-10-3 16:18:00',1,'Hello world!',0,0);
-insert into PostOfUsers values(8,4,'2014-10-3 16:18:00',0,'Hello!',1,7);
+insert into PostOfUsers values(7,4,'2014-10-3 16:18:00',1,'first','Hello world!',0,0);
+insert into PostOfUsers values(8,4,'2014-10-3 16:18:00',0,'second','Hello!',1,7);
