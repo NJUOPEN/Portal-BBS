ddcd9310a948246a83e30b921207ed97d06a7ad3
Mao-xy <15850563830@139.com>
2014-10-22 15:23:44 +0800

    修改KE.js

--- 网页样式/KE.js
+++ 网页样式/KE.js
@@ -18,83 +18,112 @@
 				});
 			});
 			
-		function save(){
+		function save(){ 
 		
-		//取得HTML内容
+		//取得HTML内容 
 		        var html = editor.html();
 	        	editor.sync();  
-	        	html = document.getElementById("editor_1").value; 
-	        	document.getElementById("content_1").value=html;
+	        	html = document.getElementById("editor_1").value;
+	        	document.getElementById("content_1").value=html; 
 				var action = document.getElementsByName("action"); //getElementsByName返回的是一个数组，因为不同元素的name可能相同
 				for(var i in action)
 				{
 					if (action[i].value=="doPost"){
-						dealWithReg(document.getElementById("title"),true);
-				   		dealWithReg(document.getElementById("content_1"));
+						dealWithTitle(document.getElementById("title"));
+				   		dealWithContent(document.getElementById("content_1"));
 				   		break;
 					}
 					else if(action[i].value=="doReply"){
-						dealWithReg(document.getElementById("content_1"));
+						dealWithContent(document.getElementById("content_1"));
 						break;						
 					}
 				}
-				alert(document.getElementById("content_1").value);//调试用，测试完成后请注释掉；下同
-				alert(document.getElementById("title").value);
+				//alert(document.getElementById("content_1").value);//调试用，测试完成后g请注释掉；下同
+				//alert(document.getElementById("title").value);
 	         	document.getElementById("form1").submit()
 	        	return true;
 				
         }
 		
-		function dealWithReg(obj,enhanced=false){
+		function dealWithContent(obj){  
 				if (!obj) return;
-		        var str =  obj.value;
-				str.replace("/<(/?html.*?)>/i", "");  //替换html标签
-				str.replace("/<(/?head.*?)>/i", ""); //替换head标签
-				str.replace("/<(/?meta.*?)>/i", "");  //替换meta标签
-				str.replace("/<(/?link.*?)>/i", "");  //替换link标签
-				str.replace("/<(/?body.*?)>/i", "");  //替换body标签
-				str.replace("/<(/?form.*?)>/i", "");  //替换form标签
-				str.replace("/<(applet.*?)>(.*?)<(/applet.*?)>/i", "");  //替换applet标签
-				str.replace("/<(/?applet.*?)>/i",""); //过滤applet标签
-                str.replace("/<(style.*?)>(.*?)<(/style.*?)>/i",""); //过滤style标签
-                str.replace("/<(/?style.*?)>/i",""); //过滤style标签
-                str.replace("/<(title.*?)>(.*?)<(/title.*?)>/i",""); //过滤title标签
-                str.replace("/<(/?title.*?)>/i",""); //过滤title标签
-                str.replace("/<(object.*?)>(.*?)<(/object.*?)>/i",""); //过滤object标签
-                str.replace("/<(/?objec.*?)>/i",""); //过滤object标签
-                str.replace("/<(noframes.*?)>(.*?)<(/noframes.*?)>/i",""); //过滤noframes标签
-                str.replace("/<(/?noframes.*?)>/i"); //过滤noframes标签
-                str.replace("/<(i?frame.*?)>(.*?)<(/i?frame.*?)>/i",""); //过滤frame标签
-                str.replace("/<(/?i?frame.*?)>/i",""); //过滤frame标签
-                str.replace("/<(script.*?)>(.*?)<(/script.*?)>/i",""); //过滤script标签
-                str.replace("/<(/?script.*?)>/i",""); //过滤script标签
+		        var str =  obj.value; 
+				str=str.replace(/&lt;\/?html.*?&gt;/gi, " ");  //替换html标签
+				str=str.replace(/&lt;\/?head.*?&gt;/gi, " ");  //替换head标签
+				str=str.replace(/&lt;\/?meta.*?&gt;/gi, " ");   //替换meta标签
+				str=str.replace(/&lt;\/?link.*?&gt;/gi, " ");  //替换link标签
+				str=str.replace(/&lt;\/?body.*?&gt;/gi, " ");   //替换body标签
+				str=str.replace(/&lt;\/?form.*?&gt;/gi, " ");  //替换form标签
+				str=str.replace(/&lt;(applet.*?)&gt;(.*?)&lt;(\/applet.*?)&gt;/gi, " ");  //替换applet标签
+				str=str.replace(/&lt;(\/?applet.*?)&gt;/gi," "); //过滤applet标签
+                str=str.replace(/&lt;(style.*?)&gt;(.*?)&lt;(\/style.*?)&gt;/gi, " "); //过滤style标签
+                str=str.replace(/&lt;(\/?style.*?)&gt;/gi," "); //过滤style标签
+                str=str.replace(/&lt;(title.*?)&gt;(.*?)&lt;(\/title.*?)&gt;/gi," "); //过滤title标签
+				str=str.replace(/&lt;(\/?title.*?)&gt;/gi," "); //过滤title标签
+                str=str.replace(/&lt;(object.*?)&gt;(.*?)&lt;(\/object.*?)&gt;/gi," "); //过滤object标签
+                str=str.replace(/&lt;(\/?object.*?)&gt;/gi," "); //过滤object标签
+                str=str.replace(/&lt;(noframes.*?)&gt;(.*?)&lt;(\/noframes.*?)&gt;/gi," "); //过滤noframes标签
+                str=str.replace(/&lt;(\/?noframes.*?)&gt;/gi," "); //过滤noframes标签
+                str=str.replace(/&lt;(i?frame.*?)&gt;(.*?)&lt;(\/?frame.*?)&gt;/gi," "); //过滤frame标签
+                str=str.replace(/&lt;(\/?i?frame.*?)&gt;/gi," "); //过滤frame标签
+                str=str.replace(/&lt;(script.*?)&gt;(.*?)&lt;(\/script.*?)&gt;/gi," "); //过滤script标签
+                str=str.replace(/&lt;(\/?script.*?)&gt;/gi," "); //过滤script标签
                 //str=preg_replace("/网页特效/i","javascript"); //过滤script标签
                 //str=preg_replace("/on([a-z]+)s*=/i","on1="); //过滤script标签
-                
-                if (enhanced)  //加强过滤，用于帖子标题等
-                {                
-                	str.replace("/<(div.*)>(.*?)<(/div.*)>/i",""); //替换div标签
-                	str.replace("/<(/?div.*)>/i",""); //过滤div标签
-                	str.replace("/<(span.*?)>(.*?)<(/span.*?)>/i",""); //替换span标签
-                	str.replace("/<(/?span.*?)>/i",""); //过滤span标签
-                	str.replace("/<(a.*?)>(.*?)<(/a.*?)>/i",""); //替换a标签
-                	str.replace("/<(/?a.*?)>/i",""); //过滤a标签
-                	str.replace("/<(font.*?)>(.*?)<(/font.*?)>/i",""); //替换font标签
-                	str.replace("/<(/?font.*?)>/i",""); //过滤font标签
-                	str.replace("/<(font.*?)>(.*?)<(/font.*?)>/i",""); //替换font标签
-                	str.replace("/<(/?font.*?)>/i",""); //过滤font标签
-                	str.replace("/<(table.*?)>(.*?)<(/table.*?)>/i",""); //替换table标签
-                	str.replace("/<(/?table.*?)>/i",""); //过滤table标签
-                	str.replace("/<(tr.*?)>(.*?)<(/tr.*?)>/i",""); //替换tr标签
-                	str.replace("/<(/?tr.*?)>/i",""); //过滤tr标签
-                	str.replace("/<(td.*?)>(.*?)<(/td.*?)>/i",""); //替换td标签
-                	str.replace("/<(/?td.*?)>/i",""); //过滤td标签
-                	str.replace("/<(ul.*?)>(.*?)<(/ul.*?)>/i",""); //替换ul标签
-                	str.replace("/<(/?ul.*?)>/i",""); //过滤ul标签
-                	str.replace("/<(li.*?)>(.*?)<(/li.*?)>/i",""); //替换li标签
-                	str.replace("/<(/?li.*?)>/i",""); //过滤li标签
-                	//TODO:添加更多可能导致标题样式被破坏的标签
-                }
-      
+               
 	            obj.value = str;
+				alert(document.getElementById("content_1").value); //测试用，调试完成请注释掉
+		}
+		
+		function dealWithTitle(obj){
+		         if (!obj) return;
+		        var str =  obj.value; 
+				str=str.replace(/<\/?html.*?>/gi, " ");  //替换html标签
+				str=str.replace(/<\/?head.*?>/gi, " ");  //替换head标签
+				str=str.replace(/<\/?meta.*?>/gi, " ");   //替换meta标签
+				str=str.replace(/<\/?link.*?>/gi, " ");  //替换link标签
+				str=str.replace(/<\/?body.*?>/gi, " ");   //替换body标签
+				str=str.replace(/<\/?form.*?>/gi, " ");  //替换form标签
+				str=str.replace(/<(applet.*?)>(.*?)<(\/applet.*?)>/gi, " ");  //替换applet标签
+				str=str.replace(/<(\/?applet.*?)>/gi," "); //过滤applet标签
+                str=str.replace(/<(style.*?)>(.*?)<(\/style.*?)>/gi, " "); //过滤style标签
+                str=str.replace(/<(\/?style.*?)>/gi," "); //过滤style标签
+                str=str.replace(/<(title.*?)>(.*?)<(\/title.*?)>/gi," "); //过滤title标签
+				str=str.replace(/<(\/?title.*?)>/gi," "); //过滤title标签
+                str=str.replace(/<(object.*?)>(.*?)<(\/object.*?)>/gi," "); //过滤object标签
+                str=str.replace(/<(\/?object.*?)>/gi," "); //过滤object标签
+                str=str.replace(/<(noframes.*?)>(.*?)<(\/noframes.*?)>/gi," "); //过滤noframes标签
+                str=str.replace(/<(\/?noframes.*?)>/gi," "); //过滤noframes标签
+                str=str.replace(/<(i?frame.*?)>(.*?)<(\/?frame.*?)>/gi," "); //过滤frame标签
+                str=str.replace(/<(\/?i?frame.*?)>/gi," "); //过滤frame标签
+                str=str.replace(/<(script.*?)>(.*?)<(\/script.*?)>/gi," "); //过滤script标签
+                str=str.replace(/<(\/?script.*?)>/gi," "); //过滤script标签
+                //str=preg_replace("/网页特效/i","javascript"); //过滤script标签
+                //str=preg_replace("/on([a-z]+)s*=/i","on1="); //过滤script标签
+                     
+                //加强过滤帖子标题					 
+                	str=str.replace(/<(div.*)>(.*?)<(\/div.*)>/gi," "); //替换div标签
+                	str=str.replace(/<(\/?div.*)>/gi," "); //过滤div标签
+                	str=str.replace(/<(span.*?)>(.*?)<(\/span.*?)>/gi," "); //替换span标签
+                	str=str.replace(/<(\/?span.*?)>/gi," "); //过滤span标签
+                	str=str.replace(/<(a.*?)>(.*?)<(\/a.*?)>/gi," "); //替换a标签
+                	str=str.replace(/<(\/?a.*?)>/gi," "); //过滤a标签
+                	str=str.replace(/<(font.*?)>(.*?)<(\/font.*?)>/gi," "); //替换font标签
+                	str=str.replace(/<(\/?font.*?)>/gi," "); //过滤font标签
+                	str=str.replace(/<(font.*?)>(.*?)<(\/font.*?)>/gi," "); //替换font标签
+                	str=str.replace(/<(\/?font.*?)>/gi," "); //过滤font标签
+                	str=str.replace(/<(table.*?)>(.*?)<(\/table.*?)>/gi," "); //替换table标签
+                	str=str.replace(/<(\/?table.*?)>/gi," "); //过滤table标签
+                	str=str.replace(/<(tr.*?)>(.*?)<(\/tr.*?)>/gi," "); //替换tr标签
+                	str=str.replace(/<(\/?tr.*?)>/gi," "); //过滤tr标签
+                	str=str.replace(/<(td.*?)>(.*?)<(\/td.*?)>/gi," "); //替换td标签
+                	str=str.replace(/<(\/?td.*?)>/gi," "); //过滤td标签
+                	str=str.replace(/<(ul.*?)>(.*?)<(\/ul.*?)>/gi," "); //替换ul标签
+                	str=str.replace(/<(\/?ul.*?)>/gi," "); //过滤ul标签
+                	str=str.replace(/<(li.*?)>(.*?)<(\/li.*?)>/gi," "); //替换li标签
+                	str=str.replace(/<(\/?li.*?)>/gi," "); //过滤li标签
+                	//TODO:添加更多可能导致标题样式被破坏的标签
+  
+	            obj.value = str;
+				alert(document.getElementById("title").value); //测试用，调试完成请注释掉
 		}
