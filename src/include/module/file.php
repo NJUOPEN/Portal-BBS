977d0a557b6185059ba1be9935d77eb58e94b2fb
henrys8 <547445609@qq.com>
2014-10-29 23:19:58 +0800

    整合上传代码

--- src/include/module/file.php
+++ src/include/module/file.php
@@ -5,11 +5,11 @@ define("USERFILE","./userfile/");
 //功能：保存文件
 //需要变量：id（用户的或帖子的）、文件名、文件类型、文件内容
 //$file_type=1,头像；$file_type=2,附件
-function save_file($id,$file_name,$file_type,$file_content)
+function save_file($id,$file_name,$file_type,&$file_content)
 {
 	$file_id=md5(time());//生成文件的md5
 	//数据库检验$file_id是否重复，若重复则重新生成，最多重新生成50次
-	//数据库写入($id,$file_id);
+	//数据库写入($id,$file_id,$file_name);
 	if($file_type==1){
 		$a=resetUserPicture($id,$file_id);
 		if($a==false)		
@@ -49,10 +49,11 @@ function remove_file($file_id)
 	}
 }
 
-function upload_file() {
+function upload_file($id,$file_type) {
     if ($_FILES["file"]["error"] > 0) {
 	echo "Error: ".$_FILES["file"]["error"]."<br />";
-    } else {
+    } 
+	else {
 	$filename = $_FILES["file"]["name"];
 	$filetype = $_FILES["file"]["type"];
 	$filesize = $_FILES["file"]["size"] / 1024;
@@ -66,7 +67,12 @@ function upload_file() {
 	 */
 
 	//TODO:确定一个保存路径,以及和数据库的配合
-	move_uploaded_file($fileaddr, BBS_ROOT."/".$filename);
+	//move_uploaded_file($fileaddr, BBS_ROOT."/".$filename);
+	$fp=fopen($filename,"rb");
+	$file_content=fread($fp,filesize($filename));
+	fclose($fp);
+	$a=save_file($id,$filename,$file_type,$file_content);
+	return $a;	
     }
 }
 ?>
