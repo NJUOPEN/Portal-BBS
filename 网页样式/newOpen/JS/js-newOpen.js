beb7ac637c77ca0698976bd2bb32ad4621df3922
Bagnogetoff <1051943988@qq.com>
2014-11-07 23:13:36 +0800

    最最醉醉最醉最

--- 网页样式/newOpen/JS/js-newOpen.js
+++ 网页样式/newOpen/JS/js-newOpen.js
@@ -1,22 +1,22 @@
 
-function func1{             //登陆
+function login_then(){             //登陆
      var loginName = document.getElementById("login-name").value;
 	 var loginPassword = document.getElementById("login-password").value;
-	 if(loginName=="123"&&loginPassword=="123456"){
-	     document.getElementById("login-field-1").style.display="none";
-		 document.getElementById("login-field-2").style.display="inline";
+	 if(/*loginName=="123"&&loginPassword=="123456"*/true){
+	     document.getElementById("login-field-1").style.visibility = "hidden";
+	     document.getElementById("login-field-2").style.visibility = "visable";
 	 }
 	 else{
-	     alert("input error");
+	     alert("账户或密码有误");
 	 }
 }
 
-function func2{          //登出
+function logout_then(){          //登出
     document.getElementById("login-field-1").style.display="inline";
 	document.getElementById("login-field-2").style.display="none";
 }
 
-function func3{
+function click(){
     var d = document.getElementById("postList-div-mark-1");
 	d.style.backgroundColor="black";
 }
