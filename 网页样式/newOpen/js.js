774e054d5972aece536fed648afbccac262422a7
Mao-xy <15850563830@139.com>
2014-11-07 21:18:58 +0800

    newopen->js

--- 网页样式/newOpen/js.js
+++ 网页样式/newOpen/js.js
@@ -1,12 +1,22 @@
 
-function func{
+function func1{             //登陆
      var loginName = document.getElementById("login-name").value;
 	 var loginPassword = document.getElementById("login-password").value;
 	 if(loginName=="123"&&loginPassword=="123456"){
-	     document.getElementById("login-field-1").style.display=none;
-		 document.getElementById("login-field-2").style.display=inline;
+	     document.getElementById("login-field-1").style.display="none";
+		 document.getElementById("login-field-2").style.display="inline";
 	 }
 	 else{
 	     alert("input error");
 	 }
+}
+
+function func2{          //登出
+    document.getElementById("login-field-1").style.display="inline";
+	document.getElementById("login-field-2").style.display="none";
+}
+
+function func3{
+    var d = document.getElementById("postList-div-mark-1");
+	d.style.backgroundColor="black";
 }
