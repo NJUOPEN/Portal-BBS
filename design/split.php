f57a5e7f7749adf03dcc20e0e014e200222e0b53
zwpwjwtz <zwpwjwtz@126.com>
2015-02-27 17:30:22 +0800

    单贴页面显示个人头像

--- design/split.php
+++ design/split.php
@@ -79,6 +79,7 @@ $replace_args=array(
 	'<span class="login-field-2-name">这里是用户名</span>' => '<span class="login-field-2-name"><?php echo $_SESSION["Name"]; ?></span>',
 	'<span class="post-login-field-2-name">这里是用户名</span>' => '<span class="post-login-field-2-name"><?php echo $_SESSION["Name"]; ?></span>',
 	'<img src="./img/headPicture-1.jpg" class="head-pic-field" />' => '<img src="<?=$_SESSION[\'avatar\'] ?>" class="head-pic-field" />',
+	'<img src="./img/headPicture-1.jpg" class="post-head-pic-field" />' => '<img src="<?=$_SESSION[\'avatar\'] ?>" class="post-head-pic-field" />',
 	'<link rel="stylesheet" href="" type="text/css">' => '',
 	'<script src="" type="text/javascript"></script>' => '',
 	'src="./' => 'src="<?php echo BBS_WEB_TEMPLATE.\'/\';?>',
