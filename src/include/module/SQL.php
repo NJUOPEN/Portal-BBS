9421801aeaa87a616cf09ddf59767183d3ba292d
zwpwjwtz <zwpwjwtz@126.com>
2015-03-03 17:15:58 +0800

    重命名SQL_Msg类函数 getTopRecord 为 getRecordRange

--- src/include/module/SQL.php
+++ src/include/module/SQL.php
@@ -294,9 +294,10 @@ class SQL_Msg extends SQL_Obj
 				condition：比较条件，可以是'> = < >= <= !='中的任何一个；
 				value：进行比较的值（string类型）；
 				//TODO：还可以添加不同比较之间的逻辑关系，如AND、OR、NOT；
+			$start:(可选)起始记录编号
 		返回值：（同getRecordByField）			
 	*/
-	protected function getTopRecord($tableName,$fieldName,$descendent=true,$count=-1,$conditions=null, $start=0)  //
+	protected function getRecordRange($tableName,$fieldName,$descendent=true,$count=-1,$conditions=null, $start=0)  
 	{
 		if (!$this->checkTable($tableName)) return NULL;
 		$query='SELECT * FROM `'.$tableName.'` ';		
@@ -413,7 +414,7 @@ class SQL_Post extends SQL_Msg //贴子操作类
 	}
 
 	public function getLastInfofPost($count, $start) {
-		return $this->getTopRecord($this->tableOfPost,'PostID',true,$count,array(array('name'=>'IfFollow','condition'=>'=','value'=>'0')), $start);
+		return $this->getRecordRange($this->tableOfPost,'PostID',true,$count,array(array('name'=>'IfFollow','condition'=>'=','value'=>'0')), $start);
 	}
 
 	public function getFollowedList($FollowAdd) {
