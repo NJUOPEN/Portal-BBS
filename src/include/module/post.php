35a78b7122cfa08e8770bb92c18aa5c7a139203f
zwpwjwtz <zwpwjwtz@126.com>
2015-04-16 21:01:35 +0800

    帖子内容页的动态分页

--- src/include/module/post.php
+++ src/include/module/post.php
@@ -28,24 +28,11 @@ function showPostList($params)
 	require_once(BBS_ROOT.'/include/module/SQL.php');
 	$PostList = new SQL_Post;
 	
-	if (isset($params['ListSize']))	//ListSize是贴子列表的分页长度
-		$listsize=(int)$params['ListSize'];
-		if ($listsize<1) $listsize=10;
-	else
-		$listsize=10;	
+	$listsize=getNatureNumber($params['ListSize'],10);	//ListSize是贴子列表的分页长度
 
-	if (isset($params['page']))	//page是浏览器请求显示的页面编号
-	{
-		$pageNum=(int)$params['page'];
-		if ($pageNum<1) $pageNum=1;
-	}
-	else
-		$pageNum=1;	
+	$pageNum=getNatureNumber($params['page'],1);	//page是浏览器请求显示的页面编号
 	
-	if ($pageNum>0)
-		$start = ($pageNum - 1) * $listsize;
-	else
-		$start=0;
+	$start = ($pageNum - 1) * $listsize;
 	
 	$post_list = $PostList->getPostList($listsize, $start);
 
@@ -73,28 +60,42 @@ function showPostView($params)
 {
 	loadUI('postView');
 	if (isset($_SESSION['SysID'])) loadUI('editor');
+	if (!isset($params['PostID'])) return;
 	
 	global $post_list;
 	require_once(BBS_ROOT.'/include/module/SQL.php');
 	$PostList = new SQL_Post;
-	if (!isset($params['ViewSize']) || $params['ViewSize']<1) $params['ViewSize']=10;	//ViewSize是回贴列表的分页长度
-	if (isset($params['PostID'])) {
+	
+	$viewsize=getNatureNumber($params['ViewSize'],10);	//ViewSize是回贴列表的分页长度
+	
+	$pageNum=getNatureNumber($params['page'],1);	//page是浏览器请求显示的页面编号
+	
+	$start = ($pageNum - 1) * $viewsize;
+	
+	
 		$post_list=array();
 		array_push($post_list,$PostList->getPost($params['PostID'])); //根据PostID获取到的帖子记录是单个元素，所以用push
 		
-		if (isset($params['replyPage']) && $params['replyPage']>0)
-			$start = ($params['replyPage'] - 1) * $params['ViewSize'];
-		else
-			$start=0;		
-		$post_list=array_merge($post_list, $PostList->getFollowedList($params['PostID'],$params['ViewSize'],$start));//而这里获取到的跟帖列表是数组，所以用merge;
+		$post_list=array_merge($post_list, $PostList->getFollowedList($params['PostID'],$viewsize,$start));//而这里获取到的跟帖列表是数组，所以用merge;
 		//print_r($post_list);
-	}
+	
 	$userDB = new SQL_User;
 	for ($i = 0; $i < count($post_list); $i++) {
 		$post_list[$i]['PostAdd'] = unEscPost($post_list[$i]['PostAdd']);
 		
 		$tempUser = $userDB->getInfOfUser($post_list[$i]['IDofUsers']);
 		$post_list[$i]['AuthorName']=$tempUser['Name'];
+	}
+	
+	global $page_link;
+	$page_link=pagination($pageNum,ceil($post_list[0]['FollowNum']/$viewsize),7);
+	for($i=0;$i<count($page_link);$i++)	//将页面编号进一步扩展成编号+链接
+	{
+		$temp=$page_link[$i];
+		if ($temp=='...')
+			$page_link[$i]=array($temp,'#');
+		else
+			$page_link[$i]=array($temp,'?action=postView&PostID='.$params['PostID'].'&page='.$temp);
 	}
 }
 // 发布帖子
@@ -113,6 +114,7 @@ function doPost($params) {
 
 function doReply($params) {
 	if (!isset($_SESSION['SysID'])) return;
+	if (!isset($params['PostID'])) return;
 	require_once(BBS_ROOT.'/include/module/SQL.php');
 	$newPost = new SQL_Post;
 	$time=gmdate(SQL_Post::SQL_DATE_FORMAT); //同上
